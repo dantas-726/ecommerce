29875a08f384271243ec150a195ad8b3e2a0b230
Pedro Henrique Dantas <40534089+dantas-726@users.noreply.github.com>
2024-04-15 11:31:04 -0300

    add carinho

--- vendor/hcodebr/php-classes/src/Model/Cart.php
+++ vendor/hcodebr/php-classes/src/Model/Cart.php
@@ -2,58 +2,58 @@
 
 namespace Hcode\Model;
 
-use \Hcode\DB\Sql;  
+use \Hcode\DB\Sql;
 use \Hcode\Model;
 use \Hcode\Mailer;
-Use \Hcode\Model\User;
+use \Hcode\Model\User;
 
+class Cart extends Model
+{
 
-class Cart extends Model {
+	const SESSION = "Cart";
+	const SESSION_ERROR = "CartError";
 
-  const SESSION = "Cart";
+	public static function getFromSession()
+	{
 
+		$cart = new Cart();
 
-  public static function getFromSession(){
+		if (isset($_SESSION[Cart::SESSION]) && (int)$_SESSION[Cart::SESSION]['idcart'] > 0) {
 
-    $cart = new Cart();
-    if (isset($_SESSION[Cart::SESSION]) && (int) $_SESSION[Cart::SESSION]['idcart'] > 0) {
-      $cart->get((int)$_SESSION[Cart::SESSION]['idcart']);
+			$cart->get((int)$_SESSION[Cart::SESSION]['idcart']);
+		} else {
 
-    } else {
+			$cart->getFromSessionID();
 
-      $cart->getFromSessionID();
+			if (!(int)$cart->getidcart() > 0) {
 
-      if (!(int)$cart->getidcart() > 0 ) {
+				$data = [
+					'dessessionid' => session_id()
+				];
 
-        $data = [
-          'dessessionid'=>session_id()
-        ];
+				if (User::checkLogin(false)) {
 
-        if (User::checkLogin(false)) {
+					$user = User::getFromSession();
 
-        $user = User::getFromSession();
-        $data['iduser'] = $user->getiduser();
+					$data['iduser'] = $user->getiduser();
+				}
 
-      }
+				$cart->setData($data);
 
-      $cart->setData($data);
-      $cart->save();
-      $cart->setToSession();
+				$cart->save();
 
+				$cart->setToSession();
+			}
+		}
 
-    }
+		return $cart;
+	}
 
-    return $cart;
+	public function setToSession()
+	{
 
-  }
-}
-
-  public function setToSession(){
-    $_SESSION[Cart::SESSION] = $this->getValues();
-    
-  }
-
-
+		$_SESSION[Cart::SESSION] = $this->getValues();
+	}
 
 	public function getFromSessionID()
 	{
@@ -68,40 +68,87 @@ class Cart extends Model {
 
 			$this->setData($results[0]);
 		}
+	}
 
-  }
+	public function get(int $idcart)
+	{
 
+		$sql = new Sql();
 
-    public function get(int $idcart){
-      $sql = new Sql;
-      $results = $sql->select("SELECT * FROM tb_carts WHERE idcart = :idcart", [
-        ':idcart'=>$idcart
-      ]);
+		$results = $sql->select("SELECT * FROM tb_carts WHERE idcart = :idcart", [
+			':idcart' => $idcart
+		]);
 
-      if(count($results) > 0 ){ 
-        $this->setData($results[0]);
+		if (count($results) > 0) {
 
-      }
+			$this->setData($results[0]);
+		}
+	}
 
-    }
+	public function save()
+	{
 
+		$sql = new Sql();
 
+		$results = $sql->select("CALL sp_carts_save(:idcart, :dessessionid, :iduser, :deszipcode, :vlfreight, :nrdays)", [
+			':idcart' => $this->getidcart(),
+			':dessessionid' => $this->getdessessionid(),
+			':iduser' => $this->getiduser(),
+			':deszipcode' => $this->getdeszipcode(),
+			':vlfreight' => $this->getvlfreight(),
+			':nrdays' => $this->getnrdays()
+		]);
 
-    public function save(){
+		$this->setData($results[0]);
+	}
 
+	public function addProduct(Product $product){
     $sql = new Sql();
-    $results = $sql->select("CALL sp_carts_save(:idcart, :dessessionid, :iduser, :deszipcode, :vlfreight, :nrdays)", [
-      ':idcart'=>$this->getidcart(),
-      ':dessessionid'=>$this->getdessessionid(),
-      ':iduser'=>$this->getiduser(),
-      ':deszipcode'=>$this->getdeszipcode(),
-      ':vlfreight'=>$this->getvlfreight(),
-      ':nrdays'=>$this->getnrdays(),
+		$sql->query("INSERT INTO tb_cartsproducts (idcart, idproduct) VALUES(:idcart, :idproduct, )", [
+			':idcart' => $this->getidcart(),
+			':idproduct' => $product->getidproduct()
+		]);
 
-    ]);
+		$this->getCalculateTotal();
+	}
 
-    $this->setData($results[0]);
+	public function removeProduct(Product $product, $all = false){
+
+		$sql = new Sql();
+		if ($all) {
+
+			$sql->query("UPDATE tb_cartsproducts SET dtremoved = NOW() WHERE idcart = :idcart AND idproduct = :idproduct AND dtremoved IS NULL", [
+				':idcart' => $this->getidcart(),
+				':idproduct' => $product->getidproduct()
+			]);
+		} else {
+
+			$sql->query("UPDATE tb_cartsproducts SET dtremoved = NOW() WHERE idcart = :idcart AND idproduct = :idproduct AND dtremoved IS NULL LIMIT 1", [
+				':idcart' => $this->getidcart(),
+				':idproduct' => $product->getidproduct()
+			]);
+		}
+
+		$this->getCalculateTotal();
+	}
+
+	public function getProducts()
+	{
+
+		$sql = new Sql();
+
+		$rows = $sql->select("
+			SELECT b.idproduct, b.desproduct , b.vlprice, b.vlwidth, b.vlheight, b.vllength, b.vlweight, b.desurl, COUNT(*) AS nrqtd, SUM(b.vlprice) AS vltotal 
+			FROM tb_cartsproducts a 
+			INNER JOIN tb_products b ON a.idproduct = b.idproduct 
+			WHERE a.idcart = :idcart AND a.dtremoved IS NULL 
+			GROUP BY b.idproduct, b.desproduct , b.vlprice, b.vlwidth, b.vlheight, b.vllength, b.vlweight, b.desurl 
+			ORDER BY b.desproduct
+		", [
+			':idcart' => $this->getidcart()
+		]);
+
+		return Product::checkList($rows);
+	}
+
 }
-}
-
-
